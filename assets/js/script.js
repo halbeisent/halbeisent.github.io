8720aa1dce1e6fef979c52f86eeff2be2ea1090f
halbeisent <dev@halbeisen.fr>
2018-05-11 15:58:22 +0200

    Ajout du scroll jusqu'a chaque section du CV // Added scroll to all resume sections

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
+//Effet scroll jusque chaque section du cv
+$(function() {
+  var scrollToAnchor = function( id ) {
+    var elem = $("section[id='"+ id +"']");
+    if ( typeof elem.offset()  === "undefined" ) {
+		elem = $("#"+id); }
+    if ( typeof elem.offset()  !== "undefined" ) {
+      $('html, body').animate({
+              scrollTop: elem.offset().top }, 1500 );}
+  };
+  $("a").click(function( event ) {
+    if ( $(this).attr("href").match("#") ) {
+      event.preventDefault();
+      var href = $(this).attr('href').replace('#', '')
+      scrollToAnchor( href ); }
+  });
+});
+
 // Initialisation de Materialize
   $(document).ready(function(){
     $('.sidenav').sidenav();
