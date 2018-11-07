58cc2963a4bf6f9e3f4872bd2b2d533c7b6a3695
halbeisent <dev@halbeisen.fr>
2018-11-07 10:03:55 +0100

    Mise à jour du PDF de l'exemplaire papier et correction de l'alignement

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,29 @@
 //Effet scroll jusque chaque section du cv
-$(function() {
-  var scrollToAnchor = function( id ) {
-    var elem = $("section[id='"+ id +"']");
-    if ( typeof elem.offset()  === "undefined" ) {
-		elem = $("#"+id); }
-    if ( typeof elem.offset()  !== "undefined" ) {
-      $('html, body').animate({
-              scrollTop: elem.offset().top }, 1500 );}
-  };
-  $("a").click(function( event ) {
-    if ( $(this).attr("href").match("#") ) {
-      event.preventDefault();
-      var href = $(this).attr('href').replace('#', '')
-      scrollToAnchor( href ); }
-  });
+$(function () {
+    var scrollToAnchor = function (id) {
+        var elem = $("section[id='" + id + "']");
+        if (typeof elem.offset() === "undefined") {
+            elem = $("#" + id);
+        }
+        if (typeof elem.offset() !== "undefined") {
+            $('html, body').animate({
+                scrollTop: elem.offset().top}, 1500);
+        }
+    };
+    $("a").click(function (event) {
+        if ($(this).attr("href").match("#")) {
+            event.preventDefault();
+            var href = $(this).attr('href').replace('#', '')
+            scrollToAnchor(href);
+        }
+    });
 });
 
 // Initialisation de Materialize
-  $(document).ready(function(){
+$(document).ready(function () {
     $('.sidenav').sidenav();
     $('.parallax').parallax();
     $('.collapsible').collapsible();
     $('.slider').slider();
-  });
+    // Initialisation de nicescroll
+});
